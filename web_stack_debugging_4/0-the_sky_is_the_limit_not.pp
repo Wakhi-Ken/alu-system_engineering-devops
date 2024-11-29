d50134a8e5260ef8d59034a865df40dfa3c1236e
Wakhi-Ken <w.dlamini@alustudent.com>
2024-11-29 05:05:08 -0800

    puppet

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
-i# fix our stack so that we get to 0 errors
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+# fix our stack so that we get to 0 errors
+exec { 'increase-ulimit':
+  command  => 'bin/sed -i "s/ULIMIT=" -n 15/ULIMIT=" -n 4096/\' /etc/default/nginx', 
+
+exec {restart-nginx':
+	command => 'usr/sbin/service nginx restart'
 }
