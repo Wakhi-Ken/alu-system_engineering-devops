f3fbdd9f7d7d72152bc5d6c02b67b7584eff80ac
Wakhi-Ken <w.dlamini@alustudent.com>
2024-11-29 23:08:04 -0800

    debb

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
-# fix our stack so that we get to 0 errors
-exec { 'increase-ulimit':
-  command  => 'bin/sed -i "s/ULIMIT=" -n 15/ULIMIT=" -n 4096/\' /etc/default/nginx', 
-
-exec {restart-nginx':
-	command => 'usr/sbin/service nginx restart'
+#!/usr/bin/env bash
+# increase upper limit sure
+exec { 'upperlimit' :
+  command =>  'sed -i "s/15/20000/g" /etc/default/nginx;service nginx restart',
+  path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  =>  'test -e /etc/default/nginx',
 }
